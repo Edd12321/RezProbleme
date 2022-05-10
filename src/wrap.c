3eb62785e57db9d24ac80f61b97eef07199ca749
[GPL]Ed <sanduedi309@gmail.com>
2022-05-10 10:22:36 +0000

    wrap.c

--- src/wrap.c
+++ src/wrap.c
@@ -5,33 +5,45 @@
 /** Scop: execvp(3) **/
 #include <unistd.h>
 
+/** Scop: flag **/
+#include <fcntl.h>
+
+/** Scop: boolean **/
+#include <stdbool.h>
+
+int i, oo;
+
 int
 main(int argc, char *argv[])
 {
-  /** tot in whitelist **/
+  /* tot in whitelist */
   scmp_filter_ctx filtru = seccomp_init(SCMP_ACT_ALLOW);
 
-  /* blocare acces fisiere */
-  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(fstat), 0);
-  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(unlink), 0);
-  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(unlinkat), 0);
-  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(stat), 0);
-  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(open), 0);
 
+  int blocare[] = {
+                /** fisiere baza **/
+                SCMP_SYS(unlink), SCMP_SYS(unlinkat), SCMP_SYS(open),
 
-  /* blocare acces retea */
-  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(socket), 0);
-  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(socketpair), 0);
-  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(getsockopt), 0);
-  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(getsockname), 0);
-  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(getpeername), 0);
-  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(bind), 0);
-  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(listen), 0);
-  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(accept), 0);
-  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(accept4), 0);
-  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(connect), 0);
-  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(shutdown), 0);
+                /** blocare retea **/
+                SCMP_SYS(socket), SCMP_SYS(socketpair), SCMP_SYS(getsockopt), SCMP_SYS(getsockname), SCMP_SYS(getpeername),
+                SCMP_SYS(bind), SCMP_SYS(listen), SCMP_SYS(accept), SCMP_SYS(accept4), SCMP_SYS(connect), SCMP_SYS(shutdown),
 
+                /** threading **/
+                SCMP_SYS(kill), SCMP_SYS(vfork), SCMP_SYS(fork), SCMP_SYS(clone)
+  };
+
+  oo = sizeof(blocare) / sizeof(int);
+  for (; i < oo; ++i) {
+    seccomp_rule_add(filtru, SCMP_ACT_KILL, blocare[i], false);
+  }
+
+  /* open(2) w/rw */
+  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(openat), true,
+      SCMP_CMP(2, SCMP_CMP_MASKED_EQ,O_WRONLY,O_WRONLY));
+
+  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(openat), true, 
+      SCMP_CMP(2,SCMP_CMP_MASKED_EQ,O_RDWR,O_RDWR));
+    
   seccomp_load(filtru);
 
   execvp(argv[1], &argv[1]);
