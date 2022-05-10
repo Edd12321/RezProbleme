f82f7817ce25059ce5cb46eeb846c6862c9ad72e
[GPL]Ed <sanduedi309@gmail.com>
2022-05-10 09:38:18 +0000

    Reimplementare securitate

--- src/wrap.c
+++ src/wrap.c
@@ -8,11 +8,32 @@
 int
 main(int argc, char *argv[])
 {
-  scmp_filter_ctx filtru = seccomp_init(SCMP_ACT_KILL);
-  seccomp_rule_add(filtru, SCMP_ACT_ALLOW, SCMP_SYS(read), 0);
-  seccomp_rule_add(filtru, SCMP_ACT_ALLOW, SCMP_SYS(write), 0);
-  seccomp_rule_add(filtru, SCMP_ACT_ALLOW, SCMP_SYS(exit), 0);
-  seccomp_rule_add(filtru, SCMP_ACT_ALLOW, SCMP_SYS(rt_sigreturn), 0);
+  /** tot in whitelist **/
+  scmp_filter_ctx filtru = seccomp_init(SCMP_ACT_ALLOW);
+
+  /* blocare acces fisiere */
+  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(fstat), 0);
+  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(unlink), 0);
+  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(unlinkat), 0);
+  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(stat), 0);
+  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(open), 0);
+
+
+  /* blocare acces retea */
+  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(socket), 0);
+  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(socketpair), 0);
+  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(getsockopt), 0);
+  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(getsockname), 0);
+  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(getpeername), 0);
+  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(bind), 0);
+  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(listen), 0);
+  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(accept), 0);
+  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(accept4), 0);
+  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(connect), 0);
+  seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(shutdown), 0);
+
+  seccomp_load(filtru);
 
   execvp(argv[1], &argv[1]);
+  seccomp_release(filtru);
 }
