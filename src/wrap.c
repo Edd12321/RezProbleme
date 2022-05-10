ed9134afff9b0ef3a7d36302deeaacaf7e104993
[GPL]Ed <sanduedi309@gmail.com>
2022-05-10 19:33:47 +0000

    Patch

--- src/wrap.c
+++ src/wrap.c
@@ -1,11 +1,10 @@
 #include <linux/seccomp.h>
+#include <sys/resource.h>
 #include <seccomp.h>
 #include <sys/prctl.h>
 
-/** Scop: execvp(3) **/
+/** Scop: execvp(3), flaf **/
 #include <unistd.h>
-
-/** Scop: flag **/
 #include <fcntl.h>
 
 /** Scop: boolean **/
@@ -21,16 +20,18 @@ main(int argc, char *argv[])
 
 
   int blocare[] = {
-                /** fisiere baza **/
-                SCMP_SYS(unlink), SCMP_SYS(unlinkat), SCMP_SYS(open),
-                SCMP_SYS(fstat), SCMP_SYS(stat),
-
-                /** blocare retea **/
-                SCMP_SYS(socket), SCMP_SYS(socketpair), SCMP_SYS(getsockopt), SCMP_SYS(getsockname), SCMP_SYS(getpeername),
-                SCMP_SYS(bind), SCMP_SYS(listen), SCMP_SYS(accept), SCMP_SYS(accept4), SCMP_SYS(connect), SCMP_SYS(shutdown),
-
-                /** threading **/
-                SCMP_SYS(kill), SCMP_SYS(vfork), SCMP_SYS(fork), SCMP_SYS(clone)
+    /** blocare retea **/  /** threading **/  /** fisiere baza **/
+    SCMP_SYS(socket),      SCMP_SYS(kill),    SCMP_SYS(unlink),
+    SCMP_SYS(socketpair),  SCMP_SYS(fork),    SCMP_SYS(unlinkat),
+    SCMP_SYS(getsockopt),  SCMP_SYS(vfork),   SCMP_SYS(open),
+    SCMP_SYS(getsockname), SCMP_SYS(clone),   SCMP_SYS(fstat),
+    SCMP_SYS(getpeername),                    SCMP_SYS(stat),
+    SCMP_SYS(bind),
+    SCMP_SYS(listen),
+    SCMP_SYS(accept),
+    SCMP_SYS(accept4),
+    SCMP_SYS(connect),
+    SCMP_SYS(shutdown)
   };
 
   oo = sizeof(blocare) / sizeof(int);
@@ -44,9 +45,10 @@ main(int argc, char *argv[])
 
   seccomp_rule_add(filtru, SCMP_ACT_KILL, SCMP_SYS(openat), true, 
       SCMP_CMP(2,SCMP_CMP_MASKED_EQ,O_RDWR,O_RDWR));
-    
-  seccomp_load(filtru);
+  
+  alarm(3);
 
+  seccomp_load(filtru);
   execvp(argv[1], &argv[1]);
   seccomp_release(filtru);
 }
